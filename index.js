7b175dbd456fb483a3143a75c676a8b25a1bf950
walterCuz <walcruz1988.21@gmail.com>
2024-08-12 14:38:43 -0300

    modified index 12/08

--- index.js
+++ index.js
@@ -1,41 +1,73 @@
+// const express = require('express');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const connect = require("./db");
+// require('dotenv').config();
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// const PORT = process.env.PORT || 3002;
+
+// // Orígenes permitidos
+// const allowedOrigins = [
+//     'http://localhost:3000',
+//     'https://tu-frontend-en-vercel.vercel.app'  // Reemplaza esto con la URL de tu frontend en Vercel
+// ];
+
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true,
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(morgan('tiny'));
+
+// const routes = require('./routes/api');
+// const routesGastos = require('./routes/routeGastos');
+// const routeMascota = require('./routes/routeMascota');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/api', routes);
+// app.use('/api', routesGastos);
+// app.use('/api', routeMascota);
+
+// app.listen(PORT, () => {
+//     console.log(`Server is starting at ${PORT}`);
+// });
+
+
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const connect = require("./db");
 require('dotenv').config();
-const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-// Orígenes permitidos
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://tu-frontend-en-vercel.vercel.app'  // Reemplaza esto con la URL de tu frontend en Vercel
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan('tiny'));
 
 const routes = require('./routes/api');
 const routesGastos = require('./routes/routeGastos');
 const routeMascota = require('./routes/routeMascota');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+connect
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('tiny'));
+
 
 app.use('/api', routes);
 app.use('/api', routesGastos);
