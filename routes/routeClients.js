17929bf02e92ad8d6493b3e6e9e4ee202fccd465
Walcruz2021 <cruzherreria1@gmail.com>
2024-12-27 10:27:14 -0300

    delete npm multer

--- routes/routeClients.js
+++ routes/routeClients.js
@@ -1,17 +1,20 @@
-import express from "express"
-const router=express.Router()
-import {addClient,uploadClients,listClients,listClientId,deleteClient,editClient} from "../controllers/controllersClients.js"
-//import multer from 'multer'
+import express from "express";
+const router = express.Router();
+import {
+  addClient,
+  uploadClients,
+  listClients,
+  listClientId,
+  deleteClient,
+  editClient,
+} from "../controllers/controllersClients.js";
 
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
+router.get("/listClientsCompany/:id", listClients);
+router.post("/client", addClient);
 
-router.get('/listClientsCompany/:id',listClients)
-router.post('/client',addClient)
-//router.post('/importClientsExcel',upload.single('file'),uploadClients)
 //find Client by idClient
 router.get("/listClients/:idClient", listClientId);
 router.delete("/deleteClient/:idClient", deleteClient);
 router.put("/editClient/:idClient", editClient);
 
-export default router
+export default router;
