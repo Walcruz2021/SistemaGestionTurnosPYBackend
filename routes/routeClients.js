a924092bbaf1da4d305fdefecf3cf974aac72041
Walcruz2021 <cruzherreria1@gmail.com>
2024-12-27 11:09:24 -0300

    delete coment

--- routes/routeClients.js
+++ routes/routeClients.js
@@ -2,13 +2,10 @@ import express from "express"
 const router=express.Router()
 import {addClient,uploadClients,listClients,listClientId,deleteClient,editClient} from "../controllers/controllersClients.js"
 
-
-
 router.get('/listClientsCompany/:idCompany',listClients)
 router.get('/listClientsCompany/:idCompany',listClients)
 router.post('/client',addClient)
 
-//find Client by idClient
 router.get("/listClients/:idClient", listClientId);
 router.delete("/deleteClient/:idClient", deleteClient);
 router.put("/editClient/:idClient", editClient);
