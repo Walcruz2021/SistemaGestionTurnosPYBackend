5d7f1d2370a58036ed2cc0d0bf6dc6884f39eacc
Walcruz2021 <cruzherreria1@gmail.com>
2024-12-27 10:14:47 -0300

    multer commented

--- routes/routeClients.js
+++ routes/routeClients.js
@@ -1,14 +1,14 @@
 import express from "express"
 const router=express.Router()
 import {addClient,uploadClients,listClients,listClientId,deleteClient,editClient} from "../controllers/controllersClients.js"
-import multer from 'multer'
+//import multer from 'multer'
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 
 router.get('/listClientsCompany/:id',listClients)
 router.post('/client',addClient)
-router.post('/importClientsExcel',upload.single('file'),uploadClients)
+//router.post('/importClientsExcel',upload.single('file'),uploadClients)
 //find Client by idClient
 router.get("/listClients/:idClient", listClientId);
 router.delete("/deleteClient/:idClient", deleteClient);
